e0017c9bbe9fbb2f3655dc21540e9610096c658f
Central120 <81374520+Central120@users.noreply.github.com>
2021-05-07 22:57:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>HabPortals - Landing Page</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/Untitled (1).png" rel="icon">
+  <link href="assets/img/Untitled (1).png" rel="icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Satisfy" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  d-flex justify-content-center align-items-center header-transparent">
+
+    <nav class="nav-menu d-none d-lg-block">
+      <ul>
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#features">Features</a></li>
+ <li><a target="_blank" href="https://forms.gle/v9PwDtdjNusVCwoN6">Agency Pre-registration</a></li>
+      </ul>
+    </nav><!-- .nav-menu -->
+
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero"><div class="hero1">
+    <div class="hero-container">
+      <h1>HabPortals</h1>
+      <h2>The portal that brings agencies together, by <a target="_blank" href="https://habbofests.com">HabboFests</a></h2>
+      <a href="#about" class="btn-scroll scrollto" title="Scroll Down"><i class="bx bx-chevron-down"></i></a>
+    </div></div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About HabPortals Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <span>About HabPortals</span>
+          <h2>About HabPortals</h2>
+          <p>Project HabPortals is a portal that brings users together from all 9 hotels, collaborate and innovate together.</p>
+        </div>
+
+        <div class="row">
+          <div class="image col-lg-4 d-flex align-items-stretch justify-content-center justify-content-lg-start"></div>
+          <div class="col-lg-8 d-flex flex-column align-items-stretch">
+            <div class="content pl-lg-4 d-flex flex-column justify-content-center">
+              <div class="row">
+                <div class="col-lg-6">
+                  <ul>
+                    <li><i class="icofont-rounded-right"></i> <strong>Developed By:</strong> <a target="_blank" href="https://habbofests.com">HabboFests.com</a></li>
+                  </ul>
+                </div>
+              <div class="col-lg-6">
+                </div>
+              </div>
+              <div class="row mt-n4">
+                <div class="col-md-12 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-simple-smile" style="color: #20b38e;"></i>
+                    <span>Coming Soon...</span>
+                    <p><strong>HabboFests</strong> are happy to announce that HabPortals will be launching shortly, with a launch date being confirmed in a matter of weeks. HabPortals is being developed by the HabPortals team in partnership with the fansite HabboFests.</br></br>
+                    So what is HabPortals? It's essentially a portal for portals, we're creating a portal which provides agencies with a free and easy alternative to custom built portals. We're giving control to the agency owners across the globe, with global registration capabilities and integrated rewards HabPortals is the future of agency portals. Stay connected with the global community, with agency based events, a shared database system, with user track records, and transfer systems. Not to mention completed customisability to allow users to edit rank perms, capabilities as well as those of SU. Submit your own ranks, customise what those ranks should be able to do and join an ever growing community of pre-registered agencies, who are eager to build bonds with agencies just like yours around the globe, that's all 9 hotels.</p>
+                  </div>
+                </div>
+
+                <!--  <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-document-folder" style="color: #8a1ac2;"></i>
+                    <span data-toggle="counter-up"><?php 
+                    $documents = '0';
+                    echo $documents; ?></span>
+                    <p><strong>Documents</strong> stored on the panel's filesystem</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-users-alt-1" style="color: #2cbdee;"></i>
+                    <span data-toggle="counter-up"><?php 
+                    $users = '0'; 
+                    echo $users; 
+                    ?></span>
+                    <p><strong>Users</strong> currently registered to the HabPortals portal</p>
+                  </div>
+                </div>
+
+                <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
+                  <div class="count-box">
+                    <i class="icofont-award" style="color: #ffb459;"></i>
+                    <span data-toggle="counter-up"><?php
+                    $reviews = '0';
+                    echo $reviews;
+                    ?></span>
+                    <p><strong>Reviews</strong> from agencies using the HabPortals portal</p>
+                  </div>
+                </div>!-->
+              </div>
+            </div><!-- End .content-->
+
+            <div class="skills-content pl-lg-4">
+              <div class="progress">
+                <span class="skill">Backend <i class="val">80%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="80" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">Frontend <i class="val">60%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="60" aria-valuemax="100"></div>
+                </div>
+              </div>
+
+              <div class="progress">
+                <span class="skill">Testing <i class="val">20%</i></span>
+                <div class="progress-bar-wrap">
+                  <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="20" aria-valuemax="100"></div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Me Section -->
+
+    <!-- ======= Features Section ======= -->
+    <section id="features" class="services">
+      <div class="container">
+
+        <div class="section-title">
+          <span>Features</span>
+          <h2>Features</h2>
+          <p>HabPortals has a ton of features that will benefit your agency, below is just a few</p>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4 class="title"><a href="">Global</a></h4>
+              <p class="description">Interact and transfer users globally, between hotels. Block agencies and disable global features, customising transfer abilities.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">Interconnected</a></h4>
+              <p class="description">An interconnected system which stores individual user records and creates a detailed anaylsis of their agency experience.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4 class="title"><a href="">Endless Opportunities</a></h4>
+              <p class="description">Explore hundreds of features catered to different special units, ranks with a fully customisable permissions functionality.</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">User Friendly</a></h4>
+              <p class="description">Constant development of HabPortals allows us to give your users the best possible experience.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End My Services Section -->
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials">
+      <div class="container">
+
+        <div class="owl-carousel testimonials-carousel">
+
+          <div class="testimonial-item">
+            <img src="https://www.habbo.com/habbo-imaging/avatarimage?user=louisjaii&direction=3&head_direction=3&gesture=nor&action=wav" class="testimonial-img" alt="">
+            <h3>HIA Public Ambassador</h3>
+            <h4>louisjaii</h4>
+            <p>
+              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+              HF Are Very Big Agency Advocates
+              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+            </p>
+          </div>
+
+          <div class="testimonial-item">
+            <img src="https://www.habbo.com/habbo-imaging/avatarimage?user=bobjeoffALT2&direction=3&head_direction=3&gesture=nor&action=wav" class="testimonial-img"  alt="">
+            <h3>X-HDA Main Founder</h3>
+            <h4>bobjim99</h4>
+            <p>
+              <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+              All The People Are Super Nice
+              <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+            </p>
+          </div>
+
+          
+
+        </div>
+
+      </div>
+    </section><!-- End Testimonials Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <h3>HabPortals</h3>
+      <p>The portal that brings agencies together, by <a target="_blank" href="https://habbofests.com">HabboFests</a></p>
+      <div class="copyright">
+        &copy; Copyright <strong><span><a target="_blank" href="https://habbofests.com">HabboFests</a> 2018 - 2021</span></strong></br></br> HabboFests is not affiliated with, endorsed, sponsored, or specifically approved by Sulake Oy or its Affiliates. HabboFests may use the trademarks and other intellectual property of Habbo, which is permitted under the Habbo Fan Site Policy.
+      </div>
+      <div class="credits">
+      Coded with ♥️ by HabboFests</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
